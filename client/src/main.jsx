a10d803b08edcafff8f5a9b50a0be903f8745cca
prakhar2219 <welcomeprakhar@gmail.com>
2026-03-09 23:53:15 +0530

    a

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx'; // Import the default export App
-import ErrorBoundary from './common/Errorboundary.jsx';
+import ErrorBoundary from './common/ErrorBoundary.jsx';
 
 
 
